7ed40ac1f3a489466746dec9a729b88e4e01d997
Pria Admaja <pria.admaja@havedev.com>
2024-01-11 20:20:03 +0700

    fix unlimited scroll

--- src/components/juz/detail.juz.js
+++ src/components/juz/detail.juz.js
@@ -1,88 +1,90 @@
-import DetailSurah from "../surah/detail.surah"
-import { useLocation } from "react-router-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
-import { Container, DropdownButton, Dropdown, Modal } from "react-bootstrap"
-import Basmallah from "../utils/basmallah"
-import ModalTafsir from "../utils/modal"
-import Loading from "../utils/loading"
-import UpButton from "../layouts/upbutton"
-import { useDispatch } from "react-redux"
-import { pageTitleAction } from "../../redux/slices/pageTitle"
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, DropdownButton, Dropdown } from "react-bootstrap";
+
+import ModalTafsir from "../utils/modal";
+import Loading from "../utils/loading";
+import UpButton from "../layouts/upbutton";
+import { useDispatch } from "react-redux";
+import { pageTitleAction } from "../../redux/slices/pageTitle";
 
 const DetailJuz = () => {
-    const location = useLocation()
-    const [datas, setDatas] = useState([])
-    const [limit, setLimit] = useState(10)
-    const [show, setShow] = useState(false)
-    const [getTaf, setTaf] = useState()
-    const [loading, setLoading] = useState(false)
-    const dispatch = useDispatch()
+  const location = useLocation();
+  const [datas, setDatas] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [show, setShow] = useState(false);
+  const [getTaf, setTaf] = useState();
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        let isCanceled = true
-        if (isCanceled === true) {
-            setLoading(true)
-            axios({
-                method: "GET",
-                url: `${process.env.REACT_APP_BASEURL}${location.pathname}`
-            }).then((result) => setDatas(result.data)).catch((err) => console.log(err)).finally(() => setLoading(false))
-        }
-        return () => { isCanceled = false }
-    }, [])
-    datas.data?.juz && dispatch(pageTitleAction.setpageTitle(`JUZ ${datas.data?.juz}`))
-    
-    useEffect(() => {
-        const onScroll = function () {
-            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-                setLimit((prev) => prev + 10)
-            }
-        }
-        window.addEventListener('scroll', onScroll)
-        return () => window.removeEventListener('scroll', onScroll)
-    }, [])
-
-    const openModal = (t) => {
-        setTaf(t)
-        setShow(true)
-        console.log('tes');
+  useEffect(() => {
+    let isCanceled = true;
+    if (isCanceled === true) {
+      setLoading(true);
+      axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_BASEURL}${location.pathname}`,
+      })
+        .then((result) => setDatas(result.data))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
     }
+    return () => {
+      isCanceled = false;
+    };
+  }, [location.pathname]);
+  datas.data?.juz &&
+    dispatch(pageTitleAction.setpageTitle(`JUZ ${datas.data?.juz}`));
 
-    const closeModal = () => setShow(false)
+  useEffect(() => {
+    const onScroll = function () {
+      if (window.innerHeight + window.scrollY + 0.5 >= document.body.offsetHeight) {
+        setLimit((prev) => prev + 10);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    if (loading) return (<Loading />)
+  const openModal = (t) => {
+    setTaf(t);
+    setShow(true);
+  };
 
-    return (
-        <Container>
-            <center className="mt-4 mb-4">
-                <h3 className="basmalah">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ</h3>
-            </center>
-            {datas.data?.verses.map((data, i) => {
-                if (i <= limit) {
-                    return (
-                        <div key={i} className="ayat">
-                            <div className="d-flex align-items-center">
-                                <DropdownButton
-                                    variant="secondary"
-                                    title={data.number.inSurah}
-                                >
-                                    <Dropdown.Item onClick={() => openModal(data.tafsir.id.long)} >
-                                        Tafsir
-                                    </Dropdown.Item>
-                                </DropdownButton>
+  const closeModal = () => setShow(false);
 
-                            </div>
-                            <div className="arabic" >{data.text.arab}</div>
-                            <div><b>{data.text.transliteration.en}</b></div>
-                            <div>{data.translation.id}</div>
-                        </div>
-                    )
-                }
-            })}
-            <ModalTafsir show={show} onHide={closeModal} desc={getTaf} />
-            <UpButton />
-        </Container>
-    )
-}
+  if (loading) return <Loading />;
 
-export default DetailJuz
+  return (
+    <Container>
+      <center className="mt-4 mb-4">
+        <h3 className="basmalah">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ</h3>
+      </center>
+      {datas.data?.verses.map((data, i) => {
+        if (i <= limit) {
+          return (
+            <div key={i} className="ayat">
+              <div className="d-flex align-items-center">
+                <DropdownButton variant="secondary" title={data.number.inSurah}>
+                  <Dropdown.Item onClick={() => openModal(data.tafsir.id.long)}>
+                    Tafsir
+                  </Dropdown.Item>
+                </DropdownButton>
+              </div>
+              <div className="arabic">{data.text.arab}</div>
+              <div>
+                <b>{data.text.transliteration.en}</b>
+              </div>
+              <div>{data.translation.id}</div>
+            </div>
+          );
+        }
+      })}
+      <ModalTafsir show={show} onHide={closeModal} desc={getTaf} />
+      <UpButton />
+    </Container>
+  );
+};
+
+export default DetailJuz;
--- src/components/surah/detail.surah.js
+++ src/components/surah/detail.surah.js
@@ -1,91 +1,96 @@
-import { React, useEffect, useState } from "react"
-import axios from "axios"
-import { useLocation } from "react-router-dom"
-import { Container, Dropdown, DropdownButton, Button } from "react-bootstrap"
-import ModalTafsir from "../utils/modal"
-import Loading from "../utils/loading"
-import UpButton from "../layouts/upbutton"
-import { useDispatch } from "react-redux"
-import { pageTitleAction } from "../../redux/slices/pageTitle"
-
-
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { Container, Dropdown, DropdownButton } from "react-bootstrap";
+import ModalTafsir from "../utils/modal";
+import Loading from "../utils/loading";
+import UpButton from "../layouts/upbutton";
+import { useDispatch } from "react-redux";
+import { pageTitleAction } from "../../redux/slices/pageTitle";
 
 const DetailSurah = () => {
-    const location = useLocation()
-    const [datas, setDatas] = useState([])
-    const [limit, setLimit] = useState(10)
-    const [show, setShow] = useState(false)
-    const [getTaf, setTaf] = useState()
-    const [loading, setLoading] = useState(false)
-    const dispatch = useDispatch()
-    
-    useEffect(() => {
-        let isCanceled = false
-        if (isCanceled === false) {
-            setLoading(true)
-            axios({
-                method: "GET",
-                url: `${process.env.REACT_APP_BASEURL}${location.pathname}`
-            }).then((result) => setDatas(result.data)).catch((err) => console.log(err)).finally(() => setLoading(false))
-        }
-        return () => {isCanceled = true}
-    }, [])
-    datas.data?.name.transliteration.id && dispatch(pageTitleAction.setpageTitle(`Surah ${datas.data?.name.transliteration.id}`))
-    // console.log(datas.data?.name.transliteration.id);
+  const location = useLocation();
+  const [datas, setDatas] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [show, setShow] = useState(false);
+  const [getTaf, setTaf] = useState();
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const onScroll = function () {
-            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-                setLimit((prev) => prev + 10)
-            }
-        }
-        window.addEventListener('scroll', onScroll)
-        return () => window.removeEventListener('scroll', onScroll)
-    }, [])
-
-    const openModal = (d) => {
-        setTaf(d)
-        setShow(true)
+  useEffect(() => {
+    let isCanceled = false;
+    if (isCanceled === false) {
+      setLoading(true);
+      axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_BASEURL}${location.pathname}`,
+      })
+        .then((result) => setDatas(result.data))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
     }
+    return () => {
+      isCanceled = true;
+    };
+  }, [location.pathname]);
+  datas.data?.name.transliteration.id &&
+    dispatch(
+      pageTitleAction.setpageTitle(
+        `Surah ${datas.data?.name.transliteration.id}`
+      )
+    );
 
-    const closeModal = () => setShow(false)
+  useEffect(() => {
+    const onScroll = function () {
+      if (
+        window.innerHeight + window.scrollY + 0.5 >=
+        document.body.offsetHeight
+      ) {
+        setLimit((prev) => prev + 10);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-   if (loading) return (<Loading/>)
+  const openModal = (d) => {
+    setTaf(d);
+    setShow(true);
+  };
 
-    return (
-        <Container>
-            <center className="mt-4 mb-4">
-                <h3 className="basmalah">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ</h3>
-            </center>
-            {datas.data?.verses.map((data, i) => {
-                if (i <= limit) {
-                    return (
-                        <div key={i} className="ayat">
-                            <div className="d-flex">
-                                <DropdownButton
+  const closeModal = () => setShow(false);
 
-                                    variant="secondary"
-                                    // menuVariant="light"
-                                    title={data.number.inSurah}
+  if (loading) return <Loading />;
 
-                                >
-                                    <Dropdown.Item onClick={() => openModal(data.tafsir.id.long)} >
-                                        Tafsir
-                                    </Dropdown.Item>
-                                </DropdownButton>
-                                {/* <audio src={data.audio.primary} typeof="audio/mp3" controls className="ms-auto"/> */}
-                            </div>
-                            <div className="arabic" >{data.text.arab}</div>
-                            <div><b>{data.text.transliteration.en}</b></div>
-                            <div>{data.translation.id}</div>
-                        </div>
-                    )
-                }
-            })}
-            <ModalTafsir show={show} onHide={closeModal} desc={getTaf} />
-            <UpButton />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <center className="mt-4 mb-4">
+        <h3 className="basmalah">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ</h3>
+      </center>
+      {datas.data?.verses.map((data, i) => {
+        if (i <= limit) {
+          return (
+            <div key={i} className="ayat">
+              <div className="d-flex">
+                <DropdownButton variant="secondary" title={data.number.inSurah}>
+                  <Dropdown.Item onClick={() => openModal(data.tafsir.id.long)}>
+                    Tafsir
+                  </Dropdown.Item>
+                </DropdownButton>
+              </div>
+              <div className="arabic">{data.text.arab}</div>
+              <div>
+                <b>{data.text.transliteration.en}</b>
+              </div>
+              <div>{data.translation.id}</div>
+            </div>
+          );
+        }
+      })}
+      <ModalTafsir show={show} onHide={closeModal} desc={getTaf} />
+      <UpButton />
+    </Container>
+  );
+};
 
-export default DetailSurah
+export default DetailSurah;
